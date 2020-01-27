a204b53796975abde29df1555da49250fe7ecb55
cubita-io <cubitadotio@sina.com>
2020-01-27 09:37:39 +0800

    代码优化

---
+++ auth-boot/src/main/java/io/cubita/base/boot/Bootstrap.java
@@ -0,0 +1,11 @@
+package io.cubita.base.boot;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author jiawei
+ * @since 1.0.0
+ */
+public class Bootstrap {
+}
--- change_version.sh
+++ change_version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## 替换 auth-parent 为对应的项目的名称
+## 替换 base.auth.version 为对应的项目的名称
 shopt -s expand_aliases
 if [ ! -n "$1" ] ;then
 	echo "Please input a version as first argument, e.g., 3.2.2"
@@ -20,10 +22,14 @@ echo "Change version in root pom.xml ===>"
 sed "/<project /,/<packaging/ s/<version>.*<\/version>/<version>$1<\/version>/" pom.xml
 
 echo "Change version in subproject pom ===>"
-for filename in $(find ./ -name "pom.xml"); do
+for filename in $(find ./ ! -path '*/auth-parent/*' -name "pom.xml"); do
     echo "Dealing with ${filename:2}"
     sed "/<parent>/,/<\/parent>/ s/<version>$currentVersion<\/version>/<version>$1<\/version>/" $filename
 done
+sed "/<packaging>/,/<properties>/ s/<version>$currentVersion<\/version>/<version>$1<\/version>/" \
+    ./auth-parent/pom.xml
+sed "/<properties>/,/<\/properties>/ s/<base.auth.version>$currentVersion<\/base.auth.version>/<base.auth.version>$1<\/base.auth.version>/" \
+    ./auth-parent/pom.xml
 
 echo "Change version in build.gradle"
 for filename in $(find . -name "build.gradle" -mindepth 3); do
